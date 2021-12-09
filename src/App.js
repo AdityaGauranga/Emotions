568d9f11b70ea491d65e631bcbd443afc17c2558
AdityaGauranga <aaditya.sarma108@gmail.com>
2021-12-09 11:21:21 +0530

    added useState
    forgot to add useState before

--- src/App.js
+++ src/App.js
@@ -18,11 +18,24 @@ var emojiDictionary = {
   var emojisWeknow = Object.keys(emojiDictionary); // conversion of objects into array
 
   export default function App() {
+      var [meaning, setMeaning] = useState(""); // to show something in view, we use useState function
 
     function emojiInputHandler(event) { // processing part
         var userInput = event.target.value;
 
         var meaning = emojiDictionary[userInput]; // this is taking the values out of the dictionary
+
+        if (meaning === undefined) {
+            meaning = "We don't have this in our database :(";
+          }
+      
+          setMeaning(meaning); // react call to show output in the UI
+        }
+    }
+
+    function emojiClickHandler(emoji){ // processing part
+        var meaning = emojiDictionary[emoji];
+        setMeaning(meaning);    
     }
     return (
         <div className="App">
@@ -35,11 +48,14 @@ var emojiDictionary = {
           <h3> More emojis here </h3> {
               emojisWeknow.map(function (emoji) {
                   return (
-                      <span style = {{FontSize: "2rem", padding: "0.5rem", cursor: "pointer"}}, key = {emoji}> {emoji} </span>
+                      <span 
+                      onClick = {emojiClickHandler()}
+                      style = {{FontSize: "2rem", padding: "0.5rem", cursor: "pointer"}} key = {emoji}> {emoji} </span>
                   )
                   
               })
           }
         </div>
     
-    )}
+    );
+
